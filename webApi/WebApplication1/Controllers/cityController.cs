d796f9a4cf8f545f7a290892b28085b5c43a015c
abinashdhkl <abinashdhkl96@gmail.com>
2021-06-01 00:21:44 +0545

    git commit -m

---
+++ webApi/WebApplication1/Controllers/cityController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class cityController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "Butwal", "pokhara", "kathmandu","Devedaha","Nepaljung","shitalnagar" };
+        }
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "Butwal";
+        }
+    }
+}
